e267622d9a1b4fd3da659ed004cf38aaaeb4f00a
Juan Jose Restrepo <restrepotorojuanjose@gmail.com>
2022-05-09 08:02:24 -0500

    ADD: ADD gets and sets

--- model/Acta.py
+++ model/Acta.py
@@ -20,9 +20,29 @@ class Acta:
         self._observaciones_generales = ""
         self._detalle_criterio = {}
         self._agregar_detalles_criterios()
+        self._nota_final = 0
 
     def get_numero(self):
         return self._numero
+
+    def get_fecha(self):
+        return self._fecha
+
+    def get_nombre_estudiante(self):
+        return self._nombre_estudiante
+
+    def get_nota_final(self):
+        return self._nota_final
+
+    def get_jurado1(self):
+        return self._jurado1
+
+    def get_jurado2(self):
+        return self._jurado2
+
+    def get_director(self):
+        return self._director
+
 
     def set_observaciones_generales(self, observaciones_generales):
         self._observaciones_generales = observaciones_generales
