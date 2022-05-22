d9ef49c2441ebbebef99608063a1f17a471a08a5
Frank-Totti <86089081+Frank-Totti@users.noreply.github.com>
2022-05-21 22:41:00 -0500

    Add files via upload

--- model/Jurado.py
+++ model/Jurado.py
@@ -1,6 +1,4 @@
 from model.Usuario import  Usuario
-from model.PDF_fpdf import PDF
-
 
 """
 Debe haber un atributo en las actas que permita saber el estado del acta,
@@ -16,31 +14,10 @@ class Jurado(Usuario):
     def __init__(self, nombre, identificacion):
         Usuario.__init__(self, nombre, identificacion)
 
-    def exportar_acta_PDF(self, acta):
-        file = open("Acta.txt", "w")
-        file.write(f'Numero del acta : {acta.get_numero()}')
-        file.write(f'Fecha : {acta.get_fecha()}')
-        file.write(f'Periodo : {acta.get_periodo()}')
-        file.write(f'Autor : {acta.get_autor()}')
-        file.write(f'Nombre del Trabajo : {acta.get_nombre_trabajo()}')
-        file.write(f'Modalidad : {acta.get_modalidad()}')
-        file.write(f'Estado del acta : {acta.get_estado_acta()}')
-        file.write(f'Nombre del Estudiante : {acta.get_nombre_estudiante()}')
-        file.write(f'Identificación del Estudiante : {acta.get_identificacion_estudiante()}')
-        file.write(f'Director : {acta.get_director().get_nombre()}')
-        file.write(f'Codirector : {acta.get_codirector().get_nombre()}')
-        file.write(f'Jurado : {acta.get_jurado1().get_nombre()}')
-        file.write(f'Jurado : {acta.get_jurado2().get_nombre()}')
-        file.write(f'Nota Final : {acta.get_nota_final()}')
-        file.write(f'Reconocimiento : {acta.get_reconocimiento()}')
-        file.write(f'Reconocimiento : {acta.get_reconocimiento()}')
-        pdf = PDF()
-        pdf.add_page()
-        pdf.texts('Acta.txt')
-        pdf.titles(f'ACTA IDENTIFICADA CON EL NÚMERO :  {acta.get_numero()}')
-        pdf.set_author(acta.get_autor())
-        pdf.output('Acta exportada.pdf', 'F')
-        return acta
+
+
+
+
 
 
 
